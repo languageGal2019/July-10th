d9c14ce0ecbfd33011481f4b44c5fa86e8dd3552
languageGal2019 <language_gal@hotmail.com>
2019-07-11 17:45:22 -0300

    Initial commit

---
+++ myjs.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $(".photo1").click(function(){
+        window.location.href = "activities.html";
+    });
+
+    $(".contact").click(function(){
+        window.location.href = "form.html";
+    });
+
+    $(".photo2").click(function(){
+        window.location.href = "classes.html";
+    });
+
+    $(".photo3").click(function(){
+        window.location.href = "form.html";
+    });
+
+    $(".photo4").click(function(){
+        window.location.href = "about-us.html";
+    });
+});
